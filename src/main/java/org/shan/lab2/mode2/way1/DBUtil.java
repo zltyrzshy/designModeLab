9e9f28df7e5f3880f432c0936c6d1f328221b6ff
Shan <zwh.20@qq.com>
2022-10-12 09:47:11 +0800

    update format

--- src/main/java/org/shan/lab2/mode2/way1/DBUtil.java
+++ src/main/java/org/shan/lab2/mode2/way1/DBUtil.java
@@ -3,9 +3,9 @@ package org.shan.lab2.mode2.way1;
 public class DBUtil {
     private static final String[] strings = {"Alice in Garden", "Tango", "cloudy", "Windy", "a box of chocolate"};
 
-    public static void main(String[]args){
-        Encrypt encrypt=new EncryptAdapter();
-        for (String s : strings){
+    public static void main(String[] args) {
+        Encrypt encrypt = new EncryptAdapter();
+        for (String s : strings) {
             System.out.println(encrypt.encryptStrong(s));
             System.out.println(encrypt.encryptWeak(s));
 
--- src/main/java/org/shan/lab2/mode2/way1/Encrypt.java
+++ src/main/java/org/shan/lab2/mode2/way1/Encrypt.java
@@ -2,5 +2,6 @@ package org.shan.lab2.mode2.way1;
 
 interface Encrypt {
     String encryptStrong(String s);
+
     String encryptWeak(String s);
 }
