61aa9b8452f6c81f3a4d73eb308f8895eab39dc4
Shan <zwh.20@qq.com>
2022-10-13 09:53:16 +0800

    lab2 mode6 success

---
+++ src/main/java/org/shan/lab2/mode6/framework/Manager.java
@@ -0,0 +1,15 @@
+package org.shan.lab2.mode6.framework;
+
+import java.util.HashMap;
+
+public class Manager {
+    private HashMap<String, Product> showCase = new HashMap<>();
+
+    public void register(String name, Product product) {
+        showCase.put(name, product);
+    }
+
+    public Product create(String name){
+        return showCase.get(name).createClone();
+    }
+}
